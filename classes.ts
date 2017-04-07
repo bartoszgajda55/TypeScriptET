1592bfbbb7d15c13abe0ee7de778bac41774bfb1
Bartosz <bartek.g55@gmail.com>
2017-04-07 16:28:32 +0100

    Static members

--- classes.ts
+++ classes.ts
@@ -13,11 +13,21 @@ enum TodoState {
 // Definig class
 class TodoService {
 
+    static lastId: number = 0;
+
     constructor(
         private todos: Todo[]
         ) { }
 
+    add(todo: Todo) {
+        let newId = TodoService.getNextId();
+    }  
+    
     getAll() {
         return this.todos;
+    }
+
+    static getNextId() {
+        return TodoService.lastId +=1;
     }
 }
